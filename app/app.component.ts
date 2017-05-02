d053a10c70b447a4b887bc51e35332c45a98cfd5
Keith Stedman <keithstedman@gmail.com>
2017-05-02 16:39:27 -0700

    show new Recipes

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,24 +4,34 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <section>
-      <h1>Recipe</h1>
+      <h1>{{recipeTitle}}</h1>
     </section>
     <section class="container">
       <h1>App-root</h1>
       <ul>
-         <li *ngFor="let currentRecipe of recipes">{{currentRecipe.title}}</li>
+         <li (click)="showRecipe(currentRecipe)" class="btn col-md-2" *ngFor="let currentRecipe of recipes" >{{currentRecipe.title}}</li>
       </ul>
+    </section>
+    <section *ngIf="selectedRecipe">
+      <h3>{{selectedRecipe.title}}</h3>
     </section>
   `
 })
 
 export class AppComponent {
+  recipeTitle: string = "Keith's Cookbook";
 
   recipes: Recipe[] = [
     new Recipe('Cereal', 'Put Cereal in Bowl, Add Milk', 'Breakfast of champions'),
     new Recipe('Bacon', 'cook on medium', 'Breakfast of beasts'),
     new Recipe('Ham', 'cook on High', 'For Thanksgiving')
   ];
+  selectedRecipe = null;
+
+  showRecipe(clickedRecipe) {
+    console.log("clicked"+clickedRecipe);
+    this.selectedRecipe = clickedRecipe;
+  }
 }
 
 export class Recipe {
